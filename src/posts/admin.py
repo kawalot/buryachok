7f4193f15e88a6ed6c7138eb2c96d869b6b6d2f2
AleksandrPeshcherov <kawalot@gmail.com>
2018-11-09 12:00:36 +0200

    Urls and Views

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "timestamp"]
+    list_display = ["title", "updated", "timestamp"]
+    list_display_links = ["title", "updated"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
     class Meta:
         model = Post    
 
---
+++ src/posts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from posts import views
+
+from . import views
+
+urlpatterns = [
+    path('', views.post_list),
+    path('create/', views.post_create),
+    path('detail/', views.post_detail),
+    path('update/', views.post_update),
+    path('delete/', views.post_delete),
+]
