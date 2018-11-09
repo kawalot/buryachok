dd5f94e224bb53c62b52c2b644149784bda21619
AleksandrPeshcherov <kawalot@gmail.com>
2018-11-09 14:37:48 +0200

    Some changes

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,26 +1,31 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def post_create(request):
-
 	return HttpResponse("<h1>Hello from Docker</h1>")
 
 def post_detail(request):
-
-	return HttpResponse("<h1>Hello from Docker</h1>")
+	context = {
+		"title": "detail"
+	}
+	return render(request, "index.html", context)
 
 def post_list(request):
-	return render(request, "index.html", {})
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset,
+		"title": "list"
+	}
+	return render(request, "index.html", context)
 	#return HttpResponse("<h1>Hello from Docker</h1>")
 
 def post_update(request):
-
 	return HttpResponse("<h1>Hello from Docker</h1>")
 
 def post_delete(request):
-
+	
 	return HttpResponse("<h1>Hello from Docker</h1>")
