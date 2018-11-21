a9d2296ec2c2cdc2c7745983470f5f1f1d8d5c29
AleksandrPeshcherov <kawalot@gmail.com>
2018-11-21 16:41:57 +0200

    Added pagination

--- src/posts/models.py
+++ src/posts/models.py
@@ -8,6 +8,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+    class Meta:
+    	ordering = ["-timestamp", "-updated"]
+
     def __str__(self):
         return self.title
 
--- src/posts/views.py
+++ src/posts/views.py
@@ -3,56 +3,60 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 # Create your views here.
 
-
 def post_create(request):
-	form = PostForm(request.POST or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "Successfully Created")
-		return HttpResponseRedirect(instance.get_absolute_url())
-	# if request.method == "POST":
-	# 	print(request.POST)
-	context = {
-		"form": form
-	}
-	return render(request, "post_form.html", context)
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Successfully Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    # if request.method == "POST":
+    # 	print(request.POST)
+    context = {
+        "form": form
+    }
+    return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	context = {
-		"object": instance
-	}
-	return render(request, "post_detail.html", context)
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "object": instance
+    }
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
-	context = {
-		"object_list": queryset,
-		"title": "List"
-	}
-	return render(request, "post_list.html", context)
-	#return HttpResponse("<h1>Hello from Docker</h1>")
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 10)
+    page = request.GET.get('page')
+    queryset = paginator.get_page(page)
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, "post_list.html", context)
+    #return HttpResponse("<h1>Hello from Docker</h1>")
 
 def post_update(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "Saved")
-		return HttpResponseRedirect(instance.get_absolute_url())
-	context = {
-		"title": instance.title,
-		"instance": instance,
-		"form": form,
-	}
-	return render(request, "post_form.html", context)
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Saved")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	instance.delete()
-	messages.success(request, "Deleted")
-	return redirect("posts:list")
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Deleted")
+    return redirect("posts:list")
