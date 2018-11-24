fcd8dc0bb557f2e385659d805723d63b275b70ba
AleksandrPeshcherov <kawalot@gmail.com>
2018-11-25 00:00:24 +0200

    Added facebook and twitter links

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote_plus
+
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
@@ -23,8 +25,10 @@ def post_create(request):
 
 def post_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
+    share_string = quote_plus(instance.content)
     context = {
-        "object": instance
+        "object": instance,
+        "share_string": share_string,
     }
     return render(request, "post_detail.html", context)
 
