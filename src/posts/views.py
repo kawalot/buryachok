f51647e7e17a2b52734935d83d7e59c9b827a4d9
AleksandrPeshcherov <kawalot@gmail.com>
2018-12-07 17:01:50 +0200

    Added draft and publish date

--- src/posts/views.py
+++ src/posts/views.py
@@ -12,8 +12,8 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 def post_create(request):
     if not request.user.is_staff or not request.user.is_superuser:
         raise Http404
-    if not request.user.is_authenticated():
-        raise Http404
+    # if not request.user.is_authenticated():
+    #     raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
